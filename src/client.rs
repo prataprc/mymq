b9c5e17c19fe6fdd558e3a929b850919ef947127
prataprc <prataprc@gmail.com>
2022-08-03 18:43:35 +0530

    client: refactor.

--- src/client.rs
+++ src/client.rs
@@ -9,17 +9,22 @@ use std::os::unix::io::{FromRawFd, IntoRawFd};
 #[cfg(windows)]
 use std::os::unix::io::{FromRawSocket, IntoRawSocket};
 
-use std::{io, mem, net, time};
+use std::{error::Error, io, mem, net, time};
 
-use crate::{v5, ClientID, Config, MQTTRead, MQTTWrite, MqttProtocol, Packetize};
+use crate::{v5, ClientID, MQTTRead, MQTTWrite, MqttProtocol, Packetize};
 
 /// MQTT CONNECT flags and headers
 #[derive(Clone, Copy)]
 pub struct ConnectOptions {
-    pub clean_start: bool,
     pub will_qos: Option<v5::QoS>,
     pub will_retain: Option<bool>,
     pub keep_alive: u16,
+}
+
+impl Default for ConnectOptions {
+    fn default() -> ConnectOptions {
+        ConnectOptions { will_qos: None, will_retain: None, keep_alive: 0 }
+    }
 }
 
 /// ClientBuilder to create a customized [Client].
@@ -54,12 +59,7 @@ impl Default for ClientBuilder {
             nodelay: None,
             ttl: None,
             // CONNECT options
-            connopts: ConnectOptions {
-                clean_start: true,
-                will_qos: None,
-                will_retain: None,
-                keep_alive: 0,
-            },
+            connopts: ConnectOptions::default(),
             connect_properties: Some(v5::ConnectProperties::default()),
             connect_payload: v5::ConnectPayload::default(),
 
@@ -129,15 +129,156 @@ impl ClientBuilder {
     }
 
     fn to_connect(&self) -> v5::Connect {
-        let is_will = self.connect_payload.will_topic.is_some()
-            && self.connect_payload.will_properties.is_some()
-            && self.connect_payload.will_payload.is_some();
-
-        let mut flags = vec![];
-        if self.connopts.clean_start {
-            flags.push(v5::ConnectFlags::CLEAN_START);
+        let mut flags = vec![v5::ConnectFlags::CLEAN_START];
+        if self.is_will() {
+            flags.push(v5::ConnectFlags::WILL_FLAG);
+            flags.push(match self.connopts.will_qos.unwrap_or(v5::QoS::AtMostOnce) {
+                v5::QoS::AtMostOnce => v5::ConnectFlags::WILL_QOS0,
+                v5::QoS::AtLeastOnce => v5::ConnectFlags::WILL_QOS1,
+                v5::QoS::ExactlyOnce => v5::ConnectFlags::WILL_QOS2,
+            });
+            match self.connopts.will_retain {
+                Some(true) => flags.push(v5::ConnectFlags::WILL_RETAIN),
+                Some(_) | None => (),
+            }
         }
-        if is_will {
+        match &self.connect_payload.username {
+            Some(_) => flags.push(v5::ConnectFlags::USERNAME),
+            None => (),
+        }
+        match &self.connect_payload.password {
+            Some(_) => flags.push(v5::ConnectFlags::PASSWORD),
+            None => (),
+        }
+
+        let mut connect = v5::Connect {
+            protocol_name: "MQTT".to_string(),
+            protocol_version: self.protocol_version,
+            flags: v5::ConnectFlags::new(&flags),
+            keep_alive: self.connopts.keep_alive,
+            properties: self.connect_properties.clone(),
+            payload: self.connect_payload.clone(),
+        };
+        connect.normalize();
+
+        connect
+    }
+
+    fn is_will(&self) -> bool {
+        self.connect_payload.will_topic.is_some()
+            && self.connect_payload.will_properties.is_some()
+            && self.connect_payload.will_payload.is_some()
+    }
+
+    fn into_client(self, remote: net::SocketAddr) -> Client {
+        Client {
+            client_id: self.client_id.unwrap_or_else(|| ClientID::new_uuid_v4()),
+            remote,
+            protocol_version: self.protocol_version,
+            connect_timeout: self.connect_timeout,
+            read_timeout: self.read_timeout,
+            write_timeout: self.write_timeout,
+            nodelay: self.nodelay,
+            ttl: self.ttl,
+            connopts: self.connopts,
+            connect_properties: self.connect_properties.clone(),
+            connect_payload: self.connect_payload.clone(),
+            // CONNACK options
+            connack: v5::ConnAck::default(),
+
+            last_sent: time::Instant::now(),
+            last_rcvd: time::Instant::now(),
+            cio: ClientIO::None,
+        }
+    }
+}
+
+/// Type to interface with MQTT broker.
+pub struct Client {
+    client_id: ClientID,
+    remote: net::SocketAddr,
+    protocol_version: MqttProtocol,
+    connect_timeout: Option<time::Duration>,
+    read_timeout: Option<time::Duration>,
+    write_timeout: Option<time::Duration>,
+    nodelay: Option<bool>,
+    ttl: Option<u32>,
+    // CONNECT options
+    connopts: ConnectOptions,
+    connect_properties: Option<v5::ConnectProperties>,
+    connect_payload: v5::ConnectPayload,
+    // CONNACK options
+    connack: v5::ConnAck,
+
+    last_rcvd: time::Instant,
+    last_sent: time::Instant,
+    cio: ClientIO,
+}
+
+/// Client initialization and setup
+impl Client {
+    /// Call this immediately after `connect` or `connect_noblock` on the ClientBuilder,
+    /// else this call might panic. Returns a clone of underlying socket with read-only
+    /// permission. After calling this method, `self` becomes a write-only instance.
+    pub fn clone_read(&mut self) -> io::Result<Client> {
+        let cio = mem::replace(&mut self.cio, ClientIO::None);
+        let (rd_cio, wt_cio) = cio.split_sock()?;
+        let reader = Client {
+            client_id: self.client_id.clone(),
+            remote: self.remote,
+            protocol_version: self.protocol_version,
+            connect_timeout: self.connect_timeout,
+            read_timeout: self.read_timeout,
+            write_timeout: self.write_timeout,
+            nodelay: self.nodelay,
+            ttl: self.ttl,
+            // CONNECT options
+            connopts: self.connopts,
+            connect_properties: self.connect_properties.clone(),
+            connect_payload: self.connect_payload.clone(),
+            // CONNACK options
+            connack: self.connack.clone(),
+
+            last_rcvd: self.last_sent,
+            last_sent: self.last_rcvd,
+            cio: rd_cio,
+        };
+
+        let _none = mem::replace(&mut self.cio, wt_cio);
+        Ok(reader)
+    }
+
+    /// If error is detected on this `Client` instance call this method. Reconnecting
+    /// will connect with the same `remote`, either in block or no-block configuration
+    /// as before.
+    pub fn reconnect(mut self) -> io::Result<Self> {
+        let sock = match self.connect_timeout {
+            Some(timeout) => net::TcpStream::connect_timeout(&self.remote, timeout)?,
+            None => net::TcpStream::connect(&self.remote)?,
+        };
+        sock.set_read_timeout(self.read_timeout)?;
+        sock.set_write_timeout(self.write_timeout)?;
+        if let Some(nodelay) = self.nodelay {
+            sock.set_nodelay(nodelay)?
+        }
+        if let Some(ttl) = self.ttl {
+            sock.set_ttl(ttl)?
+        }
+
+        let (cio, connack) = {
+            let connect = self.to_connect();
+            let blocking = self.cio.is_blocking();
+            ClientIO::handshake(connect, sock, blocking)?
+        };
+        self.cio = cio;
+        self.connack = connack;
+
+        Ok(self)
+    }
+
+    fn to_connect(&self) -> v5::Connect {
+        let mut flags = vec![];
+        if self.is_will() {
             flags.push(v5::ConnectFlags::WILL_FLAG);
             flags.push(match self.connopts.will_qos.unwrap_or(v5::QoS::AtMostOnce) {
                 v5::QoS::AtMostOnce => v5::ConnectFlags::WILL_QOS0,
@@ -175,164 +316,6 @@ impl ClientBuilder {
         self.connect_payload.will_topic.is_some()
             && self.connect_payload.will_properties.is_some()
             && self.connect_payload.will_payload.is_some()
-    }
-
-    fn max_packet_size(&self) -> u32 {
-        match &self.connect_properties {
-            Some(props) => props.max_packet_size(),
-            None => Config::DEF_MQTT_MAX_PACKET_SIZE,
-        }
-    }
-
-    fn into_client(self, remote: net::SocketAddr) -> Client {
-        Client {
-            client_id: self.client_id.unwrap_or_else(|| ClientID::new_uuid_v4()),
-            remote,
-            protocol_version: self.protocol_version,
-            connect_timeout: self.connect_timeout,
-            read_timeout: self.read_timeout,
-            write_timeout: self.write_timeout,
-            nodelay: self.nodelay,
-            ttl: self.ttl,
-            connopts: self.connopts,
-            connect_properties: self.connect_properties.clone(),
-            connect_payload: self.connect_payload.clone(),
-            // CONNACK options
-            connack: v5::ConnAck::default(),
-
-            last_tx: time::Instant::now(),
-            last_rx: time::Instant::now(),
-            cio: ClientIO::None,
-        }
-    }
-}
-
-/// Type to interface with MQTT broker.
-pub struct Client {
-    client_id: ClientID,
-    remote: net::SocketAddr,
-    protocol_version: MqttProtocol,
-    connect_timeout: Option<time::Duration>,
-    read_timeout: Option<time::Duration>,
-    write_timeout: Option<time::Duration>,
-    nodelay: Option<bool>,
-    ttl: Option<u32>,
-    // CONNECT options
-    connopts: ConnectOptions,
-    connect_properties: Option<v5::ConnectProperties>,
-    connect_payload: v5::ConnectPayload,
-    // CONNACK options
-    connack: v5::ConnAck,
-
-    last_rx: time::Instant,
-    last_tx: time::Instant,
-    cio: ClientIO,
-}
-
-/// Client initialization and setup
-impl Client {
-    /// Call this immediately after `connect` or `connect_noblock` on the ClientBuilder,
-    /// else this call might panic. Returns a clone of underlying socket with read-only
-    /// permission. After calling this method, `self` becomes a write-only instance.
-    pub fn clone_read(&mut self) -> io::Result<Client> {
-        let cio = mem::replace(&mut self.cio, ClientIO::None);
-        let (rd_cio, wt_cio) = cio.split_sock()?;
-        let reader = Client {
-            client_id: self.client_id.clone(),
-            remote: self.remote,
-            protocol_version: self.protocol_version,
-            connect_timeout: self.connect_timeout,
-            read_timeout: self.read_timeout,
-            write_timeout: self.write_timeout,
-            nodelay: self.nodelay,
-            ttl: self.ttl,
-            // CONNECT options
-            connopts: self.connopts,
-            connect_properties: self.connect_properties.clone(),
-            connect_payload: self.connect_payload.clone(),
-            // CONNACK options
-            connack: self.connack.clone(),
-
-            last_rx: self.last_tx,
-            last_tx: self.last_rx,
-            cio: rd_cio,
-        };
-
-        let _none = mem::replace(&mut self.cio, wt_cio);
-        Ok(reader)
-    }
-
-    /// If error is detected on this `Client` instance call this method. Reconnecting
-    /// will connect with the same `remote`, either in block or no-block configuration
-    /// as before.
-    pub fn reconnect(mut self) -> io::Result<Self> {
-        self.connopts.clean_start = false;
-
-        let sock = match self.connect_timeout {
-            Some(timeout) => net::TcpStream::connect_timeout(&self.remote, timeout)?,
-            None => net::TcpStream::connect(&self.remote)?,
-        };
-        sock.set_read_timeout(self.read_timeout)?;
-        sock.set_write_timeout(self.write_timeout)?;
-        if let Some(nodelay) = self.nodelay {
-            sock.set_nodelay(nodelay)?
-        }
-        if let Some(ttl) = self.ttl {
-            sock.set_ttl(ttl)?
-        }
-
-        let (cio, connack) = {
-            let connect = self.to_connect();
-            let blocking = self.cio.is_blocking();
-            ClientIO::handshake(connect, sock, blocking)?
-        };
-        self.cio = cio;
-        self.connack = connack;
-
-        Ok(self)
-    }
-
-    fn to_connect(&self) -> v5::Connect {
-        let is_will = self.connect_payload.will_topic.is_some()
-            && self.connect_payload.will_properties.is_some()
-            && self.connect_payload.will_payload.is_some();
-
-        let mut flags = vec![];
-        if self.connopts.clean_start {
-            flags.push(v5::ConnectFlags::CLEAN_START);
-        }
-        if is_will {
-            flags.push(v5::ConnectFlags::WILL_FLAG);
-            flags.push(match self.connopts.will_qos.unwrap_or(v5::QoS::AtMostOnce) {
-                v5::QoS::AtMostOnce => v5::ConnectFlags::WILL_QOS0,
-                v5::QoS::AtLeastOnce => v5::ConnectFlags::WILL_QOS1,
-                v5::QoS::ExactlyOnce => v5::ConnectFlags::WILL_QOS2,
-            });
-            match self.connopts.will_retain {
-                Some(true) => flags.push(v5::ConnectFlags::WILL_RETAIN),
-                Some(_) | None => (),
-            }
-        }
-        match &self.connect_payload.username {
-            Some(_) => flags.push(v5::ConnectFlags::USERNAME),
-            None => (),
-        }
-        match &self.connect_payload.password {
-            Some(_) => flags.push(v5::ConnectFlags::PASSWORD),
-            None => (),
-        }
-
-        let mut pkt = v5::Connect {
-            protocol_name: "MQTT".to_string(),
-            protocol_version: self.protocol_version,
-            flags: v5::ConnectFlags::new(&flags),
-            keep_alive: self.connopts.keep_alive,
-            properties: self.connect_properties.clone(),
-            payload: self.connect_payload.clone(),
-        };
-        pkt.normalize();
-
-        pkt
     }
 }
 
@@ -383,7 +366,7 @@ impl Client {
 
     /// Return the duration since last server communication.
     pub fn elapsed(&self) -> time::Duration {
-        self.last_rx.elapsed()
+        self.last_rcvd.elapsed()
     }
 
     /// Return whether, if keep_alive non-ZERO, client's communication has exceeded 1.5
@@ -394,7 +377,7 @@ impl Client {
         } else {
             let keep_alive = u64::from(self.connopts.keep_alive);
             let micros = time::Duration::from_secs(keep_alive).as_micros() as f64;
-            ((micros * 1.5) as u128) < self.last_tx.elapsed().as_micros()
+            ((micros * 1.5) as u128) < self.last_sent.elapsed().as_micros()
         }
     }
 }
@@ -403,7 +386,7 @@ impl Client {
 impl Client {
     /// Send a PingReq to server.
     pub fn ping(&mut self) -> io::Result<()> {
-        self.last_tx = time::Instant::now();
+        self.last_sent = time::Instant::now();
         todo!()
     }
 }
@@ -464,19 +447,20 @@ impl ClientIO {
         blocking: bool,
     ) -> io::Result<(ClientIO, v5::ConnAck)> {
         let max_packet_size = connect.max_packet_size();
+
         let mut pktr = MQTTRead::new(max_packet_size);
         let mut pktw = MQTTWrite::new(&[], max_packet_size);
 
-        // handshake with remote
         pktw = write_pkt(pktw, &mut sock, v5::Packet::Connect(connect))?;
+
         let (val, connack) = match read_pkt(pktr, &mut sock)? {
-            (val, v5::Packet::ConnAck(connack)) => (val, connack),
+            (pktr, v5::Packet::ConnAck(connack)) => (pktr, connack),
             (_, pkt) => {
-                let msg =
-                    format!("unexpected packet in handshake {:?}", pkt.to_packet_type());
+                let msg = format!("unexpected in handshake {:?}", pkt.to_packet_type());
                 Err(io::Error::new(io::ErrorKind::InvalidData, msg))?
             }
         };
+
         pktr = val;
 
         let cio = match blocking {
@@ -615,8 +599,6 @@ fn write_pkt<W>(
 where
     W: io::Write,
 {
-    use std::error::Error;
-
     let blob = pkt
         .encode()
         .map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err.to_string()))?;
@@ -626,7 +608,7 @@ where
     loop {
         pktw = match pktw.write(sock) {
             Ok((pktw, false)) => break Ok(pktw),
-            Ok((pktw, true)) => pktw,
+            Ok((pktw, _would_block)) => pktw,
             Err(err) => match err.source() {
                 Some(source) => match source.downcast_ref::<io::Error>() {
                     Some(err) => break Err(err.kind().into()),
@@ -642,13 +624,12 @@ fn read_pkt<R>(mut pktr: MQTTRead, sock: &mut R) -> io::Result<(MQTTRead, v5::Pa
 where
     R: io::Read,
 {
-    use std::error::Error;
     pktr = pktr.reset();
 
     let res: io::Result<MQTTRead> = loop {
         pktr = match pktr.read(sock) {
             Ok((pktr, false)) => break Ok(pktr),
-            Ok((pktr, true)) => pktr,
+            Ok((pktr, _would_block)) => pktr,
             Err(err) => match err.source() {
                 Some(source) => match source.downcast_ref::<io::Error>() {
                     Some(err) => break Err(err.kind().into()),
